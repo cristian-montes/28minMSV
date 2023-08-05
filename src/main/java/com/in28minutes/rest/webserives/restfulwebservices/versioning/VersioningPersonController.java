c089685eababa2fa34a88c8b8295c7e0d7a75655
Cristian Montes <cmontesmujica@gmail.com>
2023-08-05 12:57:09 -0600

    added versioning optiong to the API

--- src/main/java/com/in28minutes/rest/webserives/restfulwebservices/versioning/VersioningPersonController.java
+++ src/main/java/com/in28minutes/rest/webserives/restfulwebservices/versioning/VersioningPersonController.java
@@ -15,4 +15,36 @@ public class VersioningPersonController {
     public PersonV2 getSecondVersionOfPerson(){
         return new PersonV2(new Name("Carlos", "Mujica"));
     }
+
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonRequestParameter(){
+        return new PersonV1("Carlos Mujica");
+    }
+
+    @GetMapping(path = "/person", params = "version=2")
+    public PersonV2 getSecondVersionOfPersonRequestParameter(){
+        return new PersonV2(new Name("Carlos", "Mujica"));
+    }
+
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonRequestRequestHeader(){
+        return new PersonV1("Carlos Mujica");
+    }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionOfPersonRequestRequestHeader(){
+        return new PersonV2(new Name("Carlos", "Mujica"));
+    }
+
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 getFirstVersionOfPersonRequestAcceptHeader(){
+        return new PersonV1("Carlos Mujica");
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 getSecondVersionOfPersonRequestAcceptHeader(){
+        return new PersonV2(new Name("Carlos", "Mujica"));
+    }
 }
