b032676f5b9edf11f55a397533fd10f9a5f34a53
Cristian Montes <cmontesmujica@gmail.com>
2023-08-05 10:22:40 -0600

    added swagger,content negotiation, and i18n for different lenguages

--- src/main/java/com/in28minutes/rest/webserives/restfulwebservices/helloworld/HellowWorldController.java
+++ src/main/java/com/in28minutes/rest/webserives/restfulwebservices/helloworld/HellowWorldController.java
@@ -1,13 +1,21 @@
 package com.in28minutes.rest.webserives.restfulwebservices.helloworld;
 
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HellowWorldController {
+    private MessageSource messageSource;
 
+    public HellowWorldController(MessageSource messageSource){
+        this.messageSource = messageSource;
+    }
     @GetMapping(path = "/hello-world")
     public String helloWorld(){
             return "Hello World";
@@ -23,5 +31,11 @@ public class HellowWorldController {
         return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+
+    }
 
 }
